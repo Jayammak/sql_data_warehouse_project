955ccb4db47b391dd8b971b164c14cd6018435dd
Jayammak <jayamma.k2805@gmail.com>
2025-12-15 17:46:01 +0530

    Update ddl.gold.sql

--- scripts/gold/ddl.gold.sql
+++ scripts/gold/ddl.gold.sql
@@ -94,3 +94,87 @@ ON si.sls_prd_key = gp.Product_number
 LEFT JOIN Gold.dim_v_customers gc
 ON si.sls_cust_id = gc.customer_id
 
+--=======================================================
+--Create dimention Table: gold.dim_customers 
+--=======================================================
+
+IF OBJECT_ID('gold.dim_customers', 'U') IS NOT NULL
+DROP TABLE gold.dim_customers
+CREATE TABLE gold.dim_customers
+ (
+customer_key INT,
+customer_id INT,
+customer_number NVARCHAR(50),
+first_name NVARCHAR(50),
+last_name NVARCHAR(50),
+country NVARCHAR(50),
+marital_status NVARCHAR(50),
+gender NVARCHAR(50),
+birthdate DATE,
+create_date DATE
+)
+--=======================================================
+--Inserting date into: gold.dim_customers 
+--=======================================================
+
+TRUNCATE TABLE gold.dim_customers
+INSERT INTO DataWarehouse.Gold.dim_customers
+SELECT * FROM gold.dim_v_customers
+
+
+--=======================================================
+--Create Dimention Table: gold.dim_products
+--=======================================================
+
+IF OBJECT_ID('gold.dim_products', 'U') IS NOT NULL
+DROP TABLE gold.dim_products
+CREATE TABLE gold.dim_products
+ (
+product_key INT,
+product_id INT,
+product_number NVARCHAR(50),
+product_name NVARCHAR(50),
+category_id NVARCHAR(50) ,
+category NVARCHAR(50),
+subcategory NVARCHAR(50),
+maintence NVARCHAR(50),
+product_cost INT,
+product_line NVARCHAR(50),
+product_start_date DATE
+)
+--=======================================================
+--Inserting date into: gold.dim_products 
+--=======================================================
+
+TRUNCATE TABLE gold.dim_products
+INSERT INTO DataWarehouse.Gold.dim_products
+select * from gold.dim_products
+
+--=======================================================
+--Create Fact Table: gold.fact_sales 
+--=======================================================
+
+IF OBJECT_ID('gold.fact_sales', 'U') IS NOT NULL
+DROP TABLE gold.fact_sales
+CREATE TABLE gold.fact_sales
+(
+order_number NVARCHAR(50),
+product_key INT,
+customer_key INT,
+order_date DATE,
+shipping_date DATE,
+due_date DATE,
+sales_amount INT,
+quantity INT,
+price INT
+)
+--=======================================================
+--Inserting date into: gold.fact_sales 
+--=======================================================
+
+TRUNCATE TABLE gold.fact_sales
+INSERT INTO DataWarehouse.gold.fact_sales
+select * from gold.fact_v_sales
+
+
+
